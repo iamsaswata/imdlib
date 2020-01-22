749fa59cfea18ea4689263b43e5e4afd3150d849
iamsaswata <nandi.pritam007@gmail.com>
2020-01-22 18:25:39 +0530

    anaconda upload 8

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ import os
 
 def test_read():
     cwd = os.path.dirname(os.path.abspath(__file__))
-    #/home/travis/build/iamsaswata/cct_nn
+    #/home/travis/build/iamsaswata/imdlib
     a = imd.open_data((2018, 2018), 'rain','yearwise', cwd)
     assert a.data.shape == (365,135,129)
 
