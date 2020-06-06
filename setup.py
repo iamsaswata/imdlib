472eb1669055926a946f31ca88eec8be474d234d
iamsaswata <nandi.pritam007@gmail.com>
2020-06-06 10:35:00 +0530

    correction for multiple year

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 
 # imdlib version
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 def readme():
     """print long description"""
@@ -20,7 +20,7 @@ def readme():
 
 setup(
     name="imdlib",
-    version="0.1.2",
+    version="0.1.3",
     author="Saswata Nandi",
     author_email="iamsaswata@yahoo.com",
     description="A tool for handling and downloading IMD gridded data",
